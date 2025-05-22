481bd2478f404231bf35caed5dfb26a303a48f0d
Takashi Mori <kanetugu2015@gmail.com>
2025-05-22 11:03:39 +0900

    add scriptdir for hakoniwa-drone-pro

--- docker/run.bash
+++ docker/run.bash
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-source docker/env.bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source ${SCRIPT_DIR}/env.bash
 
 HAKONIWA_TOP_DIR=`pwd`
-IMAGE_NAME=`cat docker/image_name.txt`
-IMAGE_TAG=`cat docker/latest_version.txt`
+IMAGE_NAME=`cat ${SCRIPT_DIR}/image_name.txt`
+IMAGE_TAG=`cat ${SCRIPT_DIR}/latest_version.txt`
 DOCKER_IMAGE=toppersjp/${IMAGE_NAME}:${IMAGE_TAG}
 
 ARCH=`arch`
-OS_TYPE=`bash docker/utils/detect_os_type.bash`
+OS_TYPE=`bash ${SCRIPT_DIR}/utils/detect_os_type.bash`
 echo $ARCH
 echo $OS_TYPE
 if [ $OS_TYPE = "Mac" ]
