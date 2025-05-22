240abb2257f599db4322203bb7bc4807947ba9ac
tmori <kanetugu2015@gmail.com>
2025-05-22 12:24:45 +0900

    add script dir on pull image

--- docker/pull-image.bash
+++ docker/pull-image.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-IMAGE_NAME=`cat docker/image_name.txt`
-IMAGE_TAG=`cat docker/latest_version.txt`
+source ${SCRIPT_DIR}/env.bash
+
+IMAGE_NAME=`cat ${SCRIPT_DIR}/image_name.txt`
+IMAGE_TAG=`cat ${SCRIPT_DIR}/latest_version.txt`
 DOCKER_IMAGE=${IMAGE_NAME}:${IMAGE_TAG}
 
 docker pull toppersjp/${DOCKER_IMAGE}
